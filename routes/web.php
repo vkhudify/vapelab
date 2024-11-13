c876a8dd5b243961c8b3a411407b3a2d83c8e232
vkhudify <vkhudify@gmail.com>
2024-11-13 10:34:00 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome')->name('welcome');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
